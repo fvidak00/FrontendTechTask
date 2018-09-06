f961034912a27b773748583edddf2b0a5283a557
FilipVidak <fvidak00@fesb.hr>
2018-09-06 21:11:44 +0200

    Click event

--- frontendtechtask/src/NewComponent/NewComponent.js
+++ frontendtechtask/src/NewComponent/NewComponent.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const newComponent = () => {
-    return(
-        <h1>Welcome to new component.</h1>
-    );
-};
+class newComponent extends Component
+{
+    constructor()
+    {
+        super();
+        this.state =
+        {
+            colorValues:
+            [
+                'black',
+                'red',
+                'blue'
+            ],
+            selectedColor: 'black',
+        };
+    }
+
+    toggleColor()
+    {
+        do
+        {
+            var item = this.state.colorValues[Math.floor(Math.random()*this.state.colorValues.length)];
+        }while(this.state.selectedColor == item);
+        this.setState({
+            selectedColor: item
+        })
+    }
+
+    toggleColorHandler = () =>
+    {
+        console.log('clicked');
+        this.toggleColor();
+    }
+
+    render()
+    {
+        const stil = 
+        {
+            color: this.state.selectedColor,
+            cursor: 'pointer'
+        }; 
+        return (
+            <h1 onClick={this.toggleColorHandler} style={stil}>Welcome to new component.</h1>
+        );
+    }
+}
 
 export default newComponent;
